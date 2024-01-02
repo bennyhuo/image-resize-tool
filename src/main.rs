9fa8ef2a451cb30b7ee2177985b80007d1e268dc
bennyhuo <bennyhuo@kotliner.cn>
2024-01-03 07:53:26 +0800

    Handle already expanded globs from shell.

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,18 @@ use wax::Glob;
 
 #[derive(Parser, Debug)]
 struct Parameter {
+    input_paths: Vec<String>,
     #[arg(long, default_value_t = 1280)]
     max_width: u32,
     #[arg(long, default_value_t = 720)]
     max_height: u32,
     #[arg(short, long)]
-    input_path: String,
-    #[arg(short, long)]
     output_dir: Option<String>,
 }
 
 fn resize_image(input_path: &str, output_path: &str, max_width: u32, max_height: u32) {
+    println!("resizing {} => {}", input_path, output_path);
+
     let input = image::open(input_path).unwrap();
     let output = input.resize(max_width, max_height, FilterType::Lanczos3);
     output.save(output_path).unwrap();
@@ -29,30 +30,32 @@ fn main() {
         fs::create_dir_all(output_dir).unwrap();
     }
 
-    let glob = Glob::new(&params.input_path).unwrap();
-    for item in glob.walk(".") {
-        let entry = item.unwrap();
-        let path = entry.path();
-        let input_path = path.to_str().unwrap();
-        let output_path = params
-            .output_dir
-            .as_ref()
-            .map_or(input_path.to_string(), |value| {
-                let input_file_name = path.file_name().unwrap().to_str().unwrap();
-                let mut path_buf = PathBuf::new();
-                path_buf.push(value);
-                path_buf
-                    .join(input_file_name)
-                    .into_os_string()
-                    .into_string()
-                    .unwrap()
-            });
+    for input_path in &params.input_paths {
+        let glob = Glob::new(input_path).unwrap();
+        for item in glob.walk(".") {
+            let entry = item.unwrap();
+            let path = entry.path();
+            let input_path = path.to_str().unwrap();
+            let output_path = params
+                .output_dir
+                .as_ref()
+                .map_or(input_path.to_string(), |value| {
+                    let input_file_name = path.file_name().unwrap().to_str().unwrap();
+                    let mut path_buf = PathBuf::new();
+                    path_buf.push(value);
+                    path_buf
+                        .join(input_file_name)
+                        .into_os_string()
+                        .into_string()
+                        .unwrap()
+                });
 
-        resize_image(
-            input_path,
-            &output_path,
-            params.max_width,
-            params.max_height,
-        )
+            resize_image(
+                input_path,
+                &output_path,
+                params.max_width,
+                params.max_height,
+            )
+        }
     }
 }
